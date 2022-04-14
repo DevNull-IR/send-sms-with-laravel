bf4b790cc84beafcfc5948a2056792ad631c33b4
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-14 16:54:16 +0430

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,8 +4,16 @@ if (! function_exists('sendCodeOTP')){
     function sendCodeOTP(int $code, $phone){
 //        https://api.codebazan.ir/sms/api.php?type=sms&apikey=[APIKEY]&code=1234&phone=09119604828
         $response = json_decode(file_get_contents('https://api.codebazan.ir/sms/api.php?type=sms&apikey=' . env('codeBazanOTP') . '&code=' . $code . '&phone=' . $phone));
-        if ($response?->result?->code == 200){
-            return true;
+        if (isset($response->result)){
+            if(isset($response->result->code)){
+                if($response->result->code == 200){
+                    return true;
+                }else {
+                    return false;
+                }
+            }else {
+                return false;
+            }
         } else {
             return false;
         }
